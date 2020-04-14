6905645487195f959614cf75ca3849f48fd23a90
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-04-14 18:03:33 -0500

    componentes lista y tabla
    probando un estilo propio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VistasComponent } from './vistas/vistas.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VistasComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,19 @@
+import { AUTOMOVILES } from "../data";
+import { Automovil } from '../models';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+  autos: Automovil[];
+
+  constructor() { }
+
+  ngOnInit()  {
+    this.autos = AUTOMOVILES; 
+  }
+
+}
