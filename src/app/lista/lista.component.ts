a7c4f6144e5eac35e2a6abd9698d23cd1058312e
NilsonCapetillo <62961588+NilsonCapetillo@users.noreply.github.com>
2020-06-18 13:46:00 -0500

    agregue imagen de background

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,7 +1,7 @@
 import { AUTOMOVILES } from "../data";
 import { Automovil } from '../models';
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
